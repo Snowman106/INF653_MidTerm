1431762f24fb1eb0a535496de1849c01c8249928
Justin Snowden <justinpsnowden@gmail.com>
2023-03-08 20:27:08 -0600

    author create

--- api/authors/create.php
+++ api/authors/create.php
@@ -1,3 +1,21 @@
-<? php_check_syntax
+<?php
 
-//
+// Check Parameters
+if(!property_exists($data, 'author')) {
+    echo json_encode(array('message' => 'Missing Required Parameters'));
+} else {
+    
+    // Set Author to Create
+    $author->author = $data->author;
+
+    // Create Author
+    $author->create();
+
+    // Create Author JSON data
+    $author_arr = array (
+        'id' => $db->lastInsertId(),
+        'author' => $author->author
+    );
+
+    print_r(json_encode($author_arr));
+}
