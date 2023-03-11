a5317f5a057b9023800d552a23614e07e51ed177
Justin Snowden <justinpsnowden@gmail.com>
2023-03-11 12:25:34 -0600

    update delete

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -1,13 +1,24 @@
 <?php
 
-// Delete Category
-$quote->delete();
+// // Delete Category
+// $quote->delete();
 
-// Create Array for JSON Data
+// // Create Array for JSON Data
+// $quote_arr = array (
+//     'id' => $quote->id
+// );
+    
+// echo json_encode($quote_arr);
+
+
+// Delete Post
+if($post->delete()){
+    // Create Array for JSON Data
 $quote_arr = array (
     'id' => $quote->id
 );
-    
-echo json_encode($quote_arr);
-
-
+    echo json_encode($quote_arr);
+    );
+} else {
+    echo json_encode('message' = 'No Quotes Found');
+}
