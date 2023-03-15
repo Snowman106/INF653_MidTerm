5ee83b274d32d215e3a3cc6c8e9f344d4eb56c8a
Justin Snowden <justinpsnowden@gmail.com>
2023-03-15 14:30:28 -0500

    removed unnneeded code and added README

--- api/authors/index.php
+++ api/authors/index.php
@@ -9,7 +9,8 @@
         exit();
     }
     
-    // Include Files    
+    // Include Files
+    // Make sure to follow capitilization for the path.    
     include_once '../../config/Database.php';
     include_once '../../Models/Author.php';
     include_once '../../functions/isValid.php';
@@ -22,16 +23,19 @@
     $author = new Author($db);
 
     // Get Raw JSON data
+    // This pulls the commands from the Raw JSON entry 
     $data = json_decode(file_get_contents("php://input"));
 
-    // Variable for isset
-    $id;
+    // // Variable for isset
+    // $id;
 
     // Get ID if Set
+    // Check if Authors exsist in the given model
     if(isset($_GET['id'])) {
         $id = $_GET['id'];
         $authorsExists = isValid($id, $author);
     } elseif (isset($data->id)) {
+        // if no specific authors are in the URL the use the data collected
         $id = $data->id;        
         $authorsExists = isValid($id, $author);
     }
@@ -39,12 +43,14 @@
     switch($method) {
         case "GET":
             if(isset($id)) {
+                // if the ID is set retrieve the specific ID
                 if(!$authorsExists){
                     echo json_encode(array('message' => 'author_id Not Found'));
                 } else {
                     include_once 'read_single.php';
                 }
             } else {
+                // No ID set retreive all
                 include_once 'read.php';
             }
             break;
